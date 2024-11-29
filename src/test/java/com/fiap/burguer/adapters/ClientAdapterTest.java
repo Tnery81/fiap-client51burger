1b860de1bf08eea436fe9f409bd313e1075cfa24
Tnery81 <doomerbr@gmail.com>
2024-11-29 20:11:09 -0300

    microservico de clientes fiapburguer testes de cobertura

--- src/test/java/com/fiap/burguer/adapters/ClientAdapterTest.java
+++ src/test/java/com/fiap/burguer/adapters/ClientAdapterTest.java
@@ -3,6 +3,7 @@ package com.fiap.burguer.adapters;
 import com.fiap.burguer.IntegrationTest;
 import com.fiap.burguer.core.domain.Client;
 import com.fiap.burguer.infraestructure.adapters.ClientAdapter;
+import com.fiap.burguer.infraestructure.entities.ClientEntity;
 import com.fiap.burguer.infraestructure.repository.ClientRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
@@ -34,6 +35,7 @@ public class ClientAdapterTest extends IntegrationTest {
     }
 
 
+    @Nested
     class UpdateClientTest extends IntegrationTest {
         @Test
         public void updateClientTest() {
@@ -49,11 +51,111 @@ public class ClientAdapterTest extends IntegrationTest {
             Assertions.assertEquals("7775829002", clientResponse.getCpf());
             Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
         }
+
+    }
+    @Test
+    public void deleteClientTest() {
+        Client client = new Client();
+        client.setName("Maria Nunes");
+        client.setCpf("7775829002");
+        client.setEmail("4fKfJ@example.com");
+
+        Client clientResponse = clientAdapter.save(client);
+        Assertions.assertNotNull(clientResponse);   }
+@Test
+    public void getClientTest() {
+
+        ClientEntity clientEntity = new ClientEntity();
+        clientEntity.setName("Maria Nunes");
+        clientEntity.setCpf("7775829002");
+        clientEntity.setEmail("4fKfJ@example.com");
+
+        clientRepository.save(clientEntity);
+        Client clientResponse = clientAdapter.findByCpf(clientEntity.getCpf());
+        Assertions.assertNotNull(clientResponse);
+        Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+        Assertions.assertEquals("7775829002", clientResponse.getCpf());
+        Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+    }
+@Test
+public void findById   () {
+
+    ClientEntity clientEntity = new ClientEntity();
+    clientEntity.setName("Maria Nunes");
+    clientEntity.setCpf("7775829002");
+    clientEntity.setEmail("4fKfJ@example.com");
+
+    clientRepository.save(clientEntity);
+    Client clientResponse = clientAdapter.findById(clientEntity.getId());
+    Assertions.assertNotNull(clientResponse);
+    Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+    Assertions.assertEquals("7775829002", clientResponse.getCpf());
+    Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+    }
+
+public void findByCpfTest() {
+    Client client = new Client();
+    client.setName("Maria Nunes");
+    client.setCpf("7775829002");
+    client.setEmail("4fKfJ@example.com");
+
+    Client clientResponse = clientAdapter.save(client);
+    Assertions.assertNotNull(clientResponse);
+    Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+    Assertions.assertEquals("7775829002", clientResponse.getCpf());
+    Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+    }
+
+public void deleteByIdTest() {
+    Client client = new Client();
+    client.setName("Maria Nunes");
+    client.setCpf("7775829002");
+    client.setEmail("4fKfJ@example.com");
+
+    Client clientResponse = clientAdapter.save(client);
+    Assertions.assertNotNull(clientResponse);
+    Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+    Assertions.assertEquals("7775829002", clientResponse.getCpf());
+    Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+    }
+
+    public void findByEmailTest() {
+        Client client = new Client();
+        client.setName("Maria Nunes");
+        client.setCpf("7775829002");
+        client.setEmail("4fKfJ@example.com");
+
+        Client clientResponse = clientAdapter.save(client);
+        Assertions.assertNotNull(clientResponse);
+        Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+        Assertions.assertEquals("7775829002", clientResponse.getCpf());
+        Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+        }
+
+        public void findAllTest() {
+            Client client = new Client();
+            client.setName("Maria Nunes");
+            client.setCpf("7775829002");
+            client.setEmail("4fKfJ@example.com");
+
+            Client clientResponse = clientAdapter.save(client);
+            Assertions.assertNotNull(clientResponse);
+            Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+            Assertions.assertEquals("7775829002", clientResponse.getCpf());
+            Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
             }
+            public void findByCpfOrEmailTest() {
+                Client client = new Client();
+                client.setName("Maria Nunes");
+                client.setCpf("7775829002");
+                client.setEmail("4fKfJ@example.com");
 
-
-
-
+                Client clientResponse = clientAdapter.save(client);
+                Assertions.assertNotNull(clientResponse);
+                Assertions.assertEquals("Maria Nunes", clientResponse.getName());
+                Assertions.assertEquals("7775829002", clientResponse.getCpf());
+                Assertions.assertEquals("4fKfJ@example.com", clientResponse.getEmail());
+                }
 
 
 }
